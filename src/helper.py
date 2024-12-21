9da74fad928dc3fb611716e16a73611c2ff7864b
Bhiki Pallai <bhikipallai@Bhikis-MacBook-Air.local>
2024-12-21 19:20:47 +0530

    for aws

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,8 @@
-from langchain.document_loaders import PyPDFDirectoryLoader
+# from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.embeddings.huggingface import HuggingFaceEmbeddings
+
 
 
 def load_data(data):
